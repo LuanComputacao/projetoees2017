8ccffad38cd6efab5884e856782f2be505b8c8aa
LuanComputacao <eng.luanroger@gmail.com>
2018-09-11 02:14:48 -0300

    starting tests

---
+++ src/test/java/com/luancomputacao/services/ProfessorServiceTest.java
@@ -0,0 +1,12 @@
+package com.luancomputacao.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProfessorServiceTest {
+
+    @Test
+    public void realizarLogin() {
+    }
+}
