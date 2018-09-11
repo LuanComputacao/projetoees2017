8f379db2c9c83e62b47dde2194e4507aeed3ebf8
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2018-09-11 02:04:01 -0300

    starting services

---
+++ src/main/java/com/luancomputacao/services/ProfessorService.java
@@ -0,0 +1,10 @@
+package com.luancomputacao.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessorService{
+    public Boolean realizarLogin() {
+        return true;
+    }
+}
