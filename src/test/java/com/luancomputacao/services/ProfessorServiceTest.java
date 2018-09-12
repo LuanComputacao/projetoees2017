ede66d223475dacde3034a95de0f875eb4ff01f3
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2018-09-11 21:57:17 -0300

    a bit of code

--- src/test/java/com/luancomputacao/services/ProfessorServiceTest.java
+++ src/test/java/com/luancomputacao/services/ProfessorServiceTest.java
@@ -1,10 +1,19 @@
 package com.luancomputacao.services;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProfessorServiceTest {
+
+    @Test
+    public void contextLoads() throws Exception {
+    }
 
     @Test
     public void realizarLogin() {
