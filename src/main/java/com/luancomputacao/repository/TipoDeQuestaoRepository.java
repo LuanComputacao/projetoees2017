7ab4321e6cee737bddcdd2e8c016e3a7992b83b6
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2018-09-12 23:56:01 -0300

    new commit

--- src/main/java/com/luancomputacao/repository/TipoDeQuestaoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.luancomputacao.repository;
-
-import com.luancomputacao.domain.TipoDeQuestao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoDeQuestaoRepository extends JpaRepository<TipoDeQuestao, Integer> {
-
-}
--- src/test/java/com/luancomputacao/services/ProfessorServiceTest.java
+++ src/test/java/com/luancomputacao/services/ProfessorServiceTest.java
@@ -1,12 +1,31 @@
 package com.luancomputacao.services;
 
+import com.luancomputacao.domain.Professor;
+import com.luancomputacao.repository.ProfessorRepository;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 
+@RunWith(SpringRunner.class)
 public class ProfessorServiceTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Test
     public void realizarLogin() {
+    }
+
+    @Test
+    public void test_verificaDados_clienteComNomeVazio() {
+
     }
 }
