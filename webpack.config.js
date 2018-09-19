5ecb36d2064666d822e10b60f4e530e1ad0b069a
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2018-09-19 03:31:42 -0300

    vai que ẽ tua tafarel

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,40 @@
 var webpack = require('webpack');
 var path = require('path');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var devMode = process.env.NODE_ENV !== 'production';
+devMode = true;
 
 module.exports = {
     mode: "development",
-    entry: './src/main/js/main.js',
+    entry: [
+        './src/main/js/main.js',
+        './src/main/css/style.scss',
+
+        ],
     output: {
-        path: path.resolve(__dirname, './src/main/webapp/static/dist/js'),
+        path: path.resolve(__dirname, './src/main/webapp/static/dist'),
         filename: 'bundle.js'
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.s[ac]ss$/,
-                use: ['css-loader', 'sass-loader']
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ],
             },
+
             {
                 test: /^\/src\/main\/js\/\.js$/,
                 exclude: /node_modules/,
