21bc51d09e5bc94d1f4eecf56b33de7866183e41
LuanComputacao <eng.luanroger@gmail.com>
2018-10-13 22:47:21 -0300

    Access to the static files

--- src/main/java/com/luancomputacao/config/SecurityConfig.java
+++ src/main/java/com/luancomputacao/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHERS = {
             "/h2-console/**",
-            "/login"
+            "/login",
+            "/static/**"
 
     };
 
