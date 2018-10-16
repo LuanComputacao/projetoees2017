7a2571f7c8924b2722a7f6f72a8e3725d061a11a
LuanComputacao <eng.luanroger@gmail.com>
2018-10-15 21:42:58 -0300

    reducing

--- src/main/java/com/luancomputacao/services/UserService.java
+++ src/main/java/com/luancomputacao/services/UserService.java
@@ -1,14 +1,8 @@
 package com.luancomputacao.services;
 
-import com.luancomputacao.domain.Professor;
-import com.luancomputacao.repository.ProfessorRepository;
 import com.luancomputacao.security.UserSS;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
 
-@Service
 public class UserService {
 
 
