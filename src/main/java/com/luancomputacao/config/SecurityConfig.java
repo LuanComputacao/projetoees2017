8315663124511af087576d57a3325d5eb794694f
LuanComputacao <eng.luanroger@gmail.com>
2018-10-18 00:09:05 -0300

    api security

--- src/main/java/com/luancomputacao/config/SecurityConfig.java
+++ src/main/java/com/luancomputacao/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     };
 
     private static final String[] PUBLIC_MATCHERS_GET = {
-            "/api/**"
+            "/api/"
     };
 
     private static final String[] PUBLIC_MATCHERS_POST = {};
